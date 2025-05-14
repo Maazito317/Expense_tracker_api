acc373a483155a39c080092b78a85c5a1c14c205
Maaz Khan <m.maazk317@gmail.com>
2025-05-14 13:40:04 -0400

    added alembic migrations

--- migrations/env.py
+++ migrations/env.py
@@ -20,7 +20,7 @@ config = context.config
 # This line sets up loggers basically.
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
-    config.set_main_option("sqlalchemy.url", DATABASE_URL)
+config.set_main_option("sqlalchemy.url", DATABASE_URL)
 
 # add your model's MetaData object here
 # for 'autogenerate' support
